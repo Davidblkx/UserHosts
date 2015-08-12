987f9f955edbfd0119523af36be6c25b5ca2605e
David Manuel Ferreira Pires <davidblkx@gmail.com>
2015-08-12 18:03:06 +0100

    add read only script

---
+++ setReadOnly.py
@@ -0,0 +1,11 @@
+import os
+
+current = os.path.dirname(os.path.abspath(__file__))
+
+for(dir, _, files) in os.walk(os.getcwd()):
+    for f in files:
+        path = os.path.join(dir, f)
+        os.chmod(path, stat.S_IREAD)
+        print(path)
+
+input();
