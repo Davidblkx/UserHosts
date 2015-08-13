b538af0baa800044e1939b317fac4e601ef49d8a
David Pires <davidblkxdeveloper@outlook.com>
2015-08-13 12:32:59 +0100

    log support

--- setReadOnly.py
+++ setReadOnly.py
@@ -1,11 +1,19 @@
-import os, stat
+import os, stat, time
 
-current = os.path.dirname(os.path.abspath(__file__))
+fi = "log.txt"
+log = open(fi, "w")
+
+log.write("\nStarting: " + time.strftime("%d/%m/%Y - %H:%M:%S"))
 
 for(dir, _, files) in os.walk(os.getcwd()):
     for f in files:
         path = os.path.join(dir, f)
+        if f == fi or f == "setReadOnly.py":
+            continue
         os.chmod(path, stat.S_IREAD)
+        log.write("\n"+path)
         print(path)
 
+log.write("\nEnd: " + time.strftime("%d/%m/%Y - %H:%M:%S"))
+log.close();
 input();
